68ff70da3ac64ea39f152bad096ee309726a16fa
Amir04sh <amirali04shakib@gmail.com>
2026-09-18 05:26:29 -0700

    Update main.js

--- main.js
+++ main.js
@@ -17,3 +17,32 @@ function calculate(){
      }
      display.value = eval(display.value);
 };
+
+
+
+function deletelast() {
+     display.value = display.value.slice (0 , -1);
+}
+
+
+const history = document.querySelector('.history');
+
+function addToHistory (expression , result) {
+     const item =
+document.createElement('p');
+
+     item.textContent = expression + ' = ' + result
+     history.appendChild(item)
+}
+
+function calculate() {
+     try {
+          const expression = display.value;
+          const result = eval(expression);
+          display.value = result;
+          addToHistory(expression , result);
+     }
+     catch {
+          display.value = 'ERROR!!'
+     }
+}
