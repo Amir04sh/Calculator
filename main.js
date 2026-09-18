a8faf6e6ee153b2d606b7276055a69fcd7cfea7d
Amir04sh <amirali04shakib@gmail.com>
2026-09-18 16:43:59 +0330

    Update main.js

--- main.js
+++ main.js
@@ -31,10 +31,6 @@ document.createElement('p');
 
      item.textContent = expression + ' = ' + result
      history.appendChild(item);
-
-     if (history.children.length > 3) {
-        history.removeChild(history.children[1]);
-    }
 }
 
 function calculate() {
